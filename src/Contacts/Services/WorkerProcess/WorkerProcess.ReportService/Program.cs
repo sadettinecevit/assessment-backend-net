ac72a3e668c0084b7337cd2c6865c6a93065c9b5
sadettinecevit <sadettinecevit@gmail.com>
2022-08-11 14:44:50 +0300

    Project tree is created with onion architecture.

---
+++ src/Contacts/Services/WorkerProcess/WorkerProcess.ReportService/Program.cs
@@ -0,0 +1,10 @@
+using WorkerProcess.ReportService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
